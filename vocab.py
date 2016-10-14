4c06dcfdf1c4d640ef0139ac3059e6d018416749
Andrew Scheller <github@loowis.durge.org>
2016-10-14 13:44:41 +0100

    Make both scripts run unmodified on both Python2 and Python3

--- vocab.py
+++ vocab.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python
 # Alex Eames RasPi.TV http://raspi.tv/?p=9652
+from __future__ import print_function
 import random
+
+try:
+   # Python2
+   input = raw_input
+except NameError:
+   # Python3
+   pass
 
 iterations = 100
 correction_factor = 6.75
-x = 0
 recognised = 0
 unknown_words = []
 
@@ -13,9 +20,10 @@ results = list(open("wordlist.txt", "r"))
 clean_lines = [x.strip() for x in results] # remove \n line ends
 lines = len(clean_lines)
 
+x = 0
 while x < iterations:
     selection = random.randint(0, lines)
-    print (clean_lines[selection])
+    print(clean_lines[selection])
     if x % 10 == 0:
         answer = input("<Enter> = recognise, anything else = don't recognise")
     else:
@@ -28,15 +36,15 @@ while x < iterations:
 
 vocab = int(recognised / float(iterations) * lines)
 
-print ("recognised: ", recognised, "out of ", iterations)
-print ("that's ", recognised / float(iterations) * 100, "%")
-print ("Your 'Scrabble list' vocabulary is: ", vocab)
-print ("Your 'Corrected'     vocabulary is: ", int(vocab / correction_factor))
-print ("Here are the words you said you didn't know...")
+print("recognised: ", recognised, "out of ", iterations)
+print("that's ", recognised / float(iterations) * 100, "%")
+print("Your 'Scrabble list' vocabulary is: ", vocab)
+print("Your 'Corrected'     vocabulary is: ", int(vocab / correction_factor))
+print("Here are the words you said you didn't know...")
 
 add_unknowns = open("unknown_words.txt", "a")
 for word in unknown_words:
-    print (word)
-    add_unknowns.write(word+"\n")    
+    print(word)
+    add_unknowns.write(word+"\n")
 
 add_unknowns.close()
