db0102e2e19b2fc53fa5c9b7336c90d20ac69607
raspitv <alex@raspi.tv>
2016-10-14 12:09:05 +0100

    amendments
    amendments

--- dict_scraper.py
+++ dict_scraper.py
@@ -1,43 +1,39 @@
 from urllib2 import Request, urlopen, URLError
 
 wordlist = list(open("unknown_words.txt", "r"))
-words = [x.strip() for x in wordlist] # remove /n line ends
+words = [x.strip() for x in wordlist]    # remove \n line ends
 
 base_url = 'http://www.dictionary.com/browse/'
 urls =[]
 
-for word in words:
+for word in words:                       # create list of urls to scan
     urls.append(base_url+word)
-
-def scan(someurl):                # page scanning function
-    # split off the last part of url and stuff it into a variable
+                                         # page scanning function
+def scan(someurl):
+    # split off the last part of url and stuff into a variable
     keyword = someurl.split('/')[4]
-    req = Request(someurl)
+    req = Request(someurl)               # grab web page
     try:
-        response = urlopen(req)
+        grab_page = urlopen(req)
     except URLError as e:
         if hasattr(e, 'reason'):
-            #print 'We failed to reach a server.'
-            #print 'Reason: ', e.reason
             print keyword, e.reason
         elif hasattr(e, 'code'):
             print 'The server couldn\'t fulfill the request.'
             print 'Error code: ', e.code
     else:
-        the_page = response.readlines()
-        for line in the_page:
-            if '<meta name="description"' in line:
-#                print line
+        web_page = grab_page.readlines()               # read web page lines
+        for line in web_page:
+            if '<meta name="description"' in line:     # find required line
                 splitline = line.split('"')
-#                print splitline
-                for entry in splitline:
+                for entry in splitline:                # extract bits we want
                     if 'definition,' in entry:
-                        #print entry
-                        #print entry.split('definition,')
-                        print keyword+":", ''.join(entry.split('definition, ')[1:])
-#                        def_unknowns = open("unknown_words_defs.txt", "a")
-#                        def_unknowns.write((keyword+":", ''.join(entry.split('definition, ')[1:])+"\n"))
-#                        def_unknowns.close()                       
+                        write_line = keyword+": "+''.join(entry.split('definition, ')[1:])
+                        print write_line
+                        write_line +="\n"           
+                        def_unknowns = open("unknown_words_defs.txt", "a")
+                        def_unknowns.write(write_line) # write word + def'n to file
+                        def_unknowns.close()                       
 for url in urls:
     scan(url)
     print
