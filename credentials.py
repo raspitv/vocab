d2947f2df01d75bbaf594964b409c84be7bd3384
raspitv <alex@raspi.tv>
2016-11-13 13:34:45 +0000

    Credentials for oAuth twitter API
    External credentials file for twitter authentication.

---
+++ credentials.py
@@ -0,0 +1,9 @@
+# Authorisation credentials for Twitter bot account
+
+# http://raspi.tv/?p=9756 and https://github.com/raspitv/vocab
+
+consumer_key    = 'insert consumer key here'
+consumer_secret = 'insert consumer secret here'
+
+access_token        = 'insert access token here'
+access_token_secret = 'insert access token secret here'
